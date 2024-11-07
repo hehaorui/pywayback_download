6c490214176af7d2a4a52b16e4656118d5825861
Haorui He <mail@hehaorui.com>
2024-11-08 01:55:28 +0800

    use httpx and asyncio
    * with client from httpx, we can reuse established connection and download more efficiently
    * with asyncio, we can avoid some multithread bug

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,85 +1,78 @@
-import requests
+from attr import has
+import httpx
 import os
 import json
-import threading
+import asyncio
+import aiofiles
 from queue import Queue
 
 
-def __download_worker__(q, failedqueue, counter, dir=""):
-  # The file is a dictionary with the keys "file_url" and "file_path"
+async def __download_coroutine__(q, failedqueue, client, dir=""):
+  if not hasattr(__download_coroutine__, "counter"):
+    __download_coroutine__.counter = 0
+  if not hasattr(__download_coroutine__, "total"):
+    __download_coroutine__.total  = q.qsize()
+  total = __download_coroutine__.total
+  
   if dir != "" and dir[-1] != "/":
     dir += "/"
-  download_total = counter["TOTAL"]
   
+  # The file is a dictionary with the keys "file_url" and "file_path"
   while not q.empty():
-    fileItem = q.get()
+    fileItem = await q.get()
     url = fileItem["file_url"]
     path = dir+fileItem["file_path"]
     
     if os.path.exists(path) and os.path.getsize(path) > 0:
-      with counter["lock"]:
-        counter["value"] += 1
-        download_count = counter["value"]
-        print(f"File already exists: {path} ({download_count}/{download_total})")
+      __download_coroutine__.counter += 1
+      print(f"File already exists: {path} ({__download_coroutine__.counter}/{total})")
       continue
 
     try:
-      r = requests.get(url, timeout=10)
+      r = await client.get(url, timeout=60)
       if r.status_code != 200:
         raise Exception(f"HTTP error: {r.status_code}")
       
       os.makedirs(os.path.dirname(path), exist_ok=True)
-      with open(path, "wb") as f:
-        f.write(r.content)
-      with counter["lock"]:
-        counter["value"] += 1
-        download_count = counter["value"]
-        print(f"Downloaded {url} to {path} ({download_count}/{download_total})")
+      async with aiofiles.open(path, "wb") as f:
+        await f.write(r.content)
+      
+      __download_coroutine__.counter += 1
+      print(f"Downloaded {url} to {path} ({__download_coroutine__.counter}/{total})")
       
     except Exception as e:
-      file_remove = os.path.exists(path) and os.path.getsize(path) == 0
-      if file_remove:
+      if os.path.exists(path) and os.path.getsize(path) == 0:
+        print(f"{path} is empty and was removed.")
         os.remove(path)
       
-      with counter["lock"]:
-        counter["value"] += 1
-        download_count = counter["value"]
-        print(f"Failed to download {url}: {e} ({download_count}/{download_total})")
-        if file_remove:
-          print(f"{path} is empty and was removed.")
-      failedqueue.put(fileItem)
+      __download_coroutine__.counter += 1
+      print(f"Failed to download {url}: {e} ({__download_coroutine__.counter}/{total})")
+      await failedqueue.put(fileItem)
 
-def download_from_filelist(files, thread_count=10, dir=""):
-  failedqueue = Queue()
-  q = Queue()
+async def download_from_filelist(files, concurrency=10, dir=""):
+  failedqueue = asyncio.Queue()
+  q = asyncio.Queue()
   for file in files:
-    q.put(file)
+    await q.put(file)
 
-  threads = []
-  counter = {"lock": threading.Lock(), "value": 0, "TOTAL": len(files)}
-
-  for _ in range(thread_count):
-    t = threading.Thread(target=__download_worker__, args=(q, failedqueue, counter, dir))
-    t.start()
-    threads.append(t)
-
-  for t in threads:
-    t.join()
-
+  async with httpx.AsyncClient(http2=True) as client:
+    coros = [__download_coroutine__(q, failedqueue, client, dir) for _ in range(concurrency)]
+    await asyncio.gather(*coros)
+  
   failedlist = []
   while not failedqueue.empty():
-    failedlist.append(failedqueue.get())
+    failedlist.append(await failedqueue.get())
 
   return failedlist
 
 
-def download_from_json(json_file, thread_count=10, parser=None, dir=""):
+def download_from_json(json_file, concurrency=10, parser=None, dir=""):
   with open(json_file, "r") as f:
     files = json.load(f)
   if parser:
     files = parser.parse(files)
 
-  failedlist = download_from_filelist(files, thread_count, dir)
+  failedlist = asyncio.run(download_from_filelist(files, concurrency, dir))
 
   with open(dir+"failed.json", "w") as f:
     json.dump(failedlist, f)
